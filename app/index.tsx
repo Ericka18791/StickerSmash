39ca2054b0c979cf162a26725e885f0c3569424b
Ericka18791 <erickajungoy41@gmail.com>
2026-07-06 14:22:33 +0800

    add 3rd number

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Princess Ericka Jungoy, 3A.</Text>
+      <Text>Mobile App Development .</Text>
+
+
+
+      
+    </View>
+  );
+}
